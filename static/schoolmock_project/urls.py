c45ef0e6c3f585f4f3be46f4b44df4654c2452ab
boburinoyatov <inoyatovboburshox99@gmail.com>
2025-02-09 14:55:30 +0500

    14:55

---
+++ static/schoolmock_project/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from schoolmock_app.urls import router
+from schoolmock_app.views import *
+from schoolmock_project import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('schoolmock_app.urls')),
+    path('api/', include(router.urls)),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # new
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # new
