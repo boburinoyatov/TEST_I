4db02c67d63c2318192ebcb8b8ae4ee0620bcf1e
boburinoyatov <inoyatovboburshox99@gmail.com>
2025-02-09 14:48:58 +0500

    14:48

--- schoolmock_project/settings.py
+++ schoolmock_project/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'static')
-STATICFILES_DIRS = [BASE_DIR / '/static/img/']
+STATICFILES_DIRS = [BASE_DIR]
 
 MEDIA_URL = '/media/'  # new
 MEDIA_ROOT = BASE_DIR, 'media'
